22f391101b29b19d1a1a6a07e9329ded6d4ed586
SophieSt <stuhlersophie@gmail.com>
2017-01-11 15:40:51 +0100

    works now, gives back correct output

--- Assignment3 KFZ-HV.R
+++ Assignment3 KFZ-HV.R
@@ -8,19 +8,31 @@
 # A leap year is by definition a year, divisible by 4,
 # not divisible by 100 except for years divisible by 400.
 
+
+
 is.leap = function (year) {
   # this function will check if the given year is/was a leap year
-  leap = FALSE
-  try_num = try(is.numeric(year), silent=TRUE)  # check for type of year, years are given in integer format
-  if (class(try_num) == 'try-error') {
-    return('year must be of type numeric')   # return a message, if year is not numeric
+  leap = FALSE   # general assumption year is not leap year, unless checked for it being so
+
+  is.num = is.numeric(year)  # check for type of year, years are given in integer format
+
+  if (!is.num) {   # return a message, if year is not numeric
+    stop ('year must be of type numeric')
+  } else if (year < 1582) {   # there were no leap years before 1582
+    message = paste(year, 'is out of valid range')
+    return (message)
   } else {
-    if ((year % 4) == 0) {
-    leap = TRUE
-    } else if ((year % 100) == 0) {
-    leap = FALSE
-    } else if ((year % 400) == 0) {
-    leap = TRUE
-  return leap
-  }
+      if ((year %% 4) == 0) {
+        leap = TRUE
+      }
+      if ((year %% 100) == 0) {
+        leap = FALSE
+      }
+      if ((year %% 400) == 0) {
+        leap = TRUE
+      }
+    }
+  return (leap)
 }
+
+is.leap(1581)
